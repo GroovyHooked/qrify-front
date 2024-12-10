a81454f62793ee18f18ea64d9612807489f15b2f
slimkhrrz <slimkhrrz@gmail.com>
2024-12-10 17:35:24 +0100

    page de connexion

--- components/signin.js
+++ components/signin.js
@@ -1,29 +1,37 @@
 import styles from "../styles/Signin.module.css";
+import Link from "next/link";
 
 function SignIn() {
   return (
-    <div className={styles.MMmain}>
-      <div className={styles.body}>
-        <div className={styles.bodyheader}>
-          <div>Connexion</div>
+    <div className={styles.container}>
+      <div className={styles.containerglobal}>
+        <div className={styles.containertitle}>
+          <div className={styles.title}>Connexion</div>
         </div>
-        <div className={styles.bodybody}>
-          <div className={styles.photo}>photo</div>
-          <div className={styles.containerconnexion}>
-            <div className={styles.containerinput}>
-              <div className={styles.inputglobal}>
-                <div className={styles.text}>Adresse Email</div>
-                <div
+        <div className={styles.containerImgForm}>
+          <div className={styles.containerImage}>photo</div>
+          <div className={styles.containerForm}>
+            <div className={styles.error}></div>
+            <div className={styles.registerSection}>
+              <div className={styles.inputgroup}>
+                <label className={styles.text}>Adresse e-mail</label>
+                <input
                   className={styles.input}
                   placeholder="Thomascarriot@gmail.com"
-                ></div>
-                <div className={styles.text}>Mot de passe</div>
-                <div className={styles.input} placeholder=""></div>
+                />
               </div>
+              <div className={styles.inputgroup}>
+                <label className={styles.text}>Mot de passe</label>
+                <input className={styles.input} placeholder="" />
+              </div>
+              <button className={styles.button} id="signUp">
+                Créer mon compte
+              </button>
             </div>
-            <div className={styles.connexion}>
-              Connectez-vous ou
-              <div className={styles.lienclic}>créez un compte</div>
+            <div className={styles.containerBottomText}>
+              <h6>
+                Connectez-vous ou <Link href="">créez un compte</Link>
+              </h6>
             </div>
           </div>
         </div>
