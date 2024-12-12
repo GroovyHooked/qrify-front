a93d3bab07f557506565f5617820505be6d43b62
myriam <chancel.my@gmail.com>
2024-12-12 17:24:24 +0100

    shareCard

--- pages/newcustomer.js
+++ pages/newcustomer.js
@@ -1,13 +1,9 @@
 import NewCustomer from "../components/newCustomer";
-import Footer from "../components/footer";
-import Navbar from "../components/navbar";
 
 function NewcustomerPage() {
   return (
     <>
-      {/* <Navbar toto="logo" /> prevoir lien vers logo du compte du commercant */}
       <NewCustomer />
-      <Footer />
     </>
   );
 }
