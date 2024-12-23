456e721f212a52507c4b87d6afbd1a929aa55d8e
groovyhooked <dafrenchie2002@yahoo.fr>
2024-12-23 11:53:23 +0100

    Commented scan component

--- components/scan.js
+++ components/scan.js
@@ -8,12 +8,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faStop, faRotate } from "@fortawesome/free-solid-svg-icons";
 
 export default function Scan() {
+
+  // État pour gérer l'orientation de la caméra (par défaut, "environment" pour la caméra arrière)
   const [camOrientation, setCamOrientation] = useState("environment");
   const router = useRouter();
 
+  // Références pour le nœud vidéo et l'objet QrScanner
   const videoNodeRef = useRef();
   const qrScannerRef = useRef(null);
 
+  /**
+   * Fonction pour changer l'orientation de la caméra
+   * 1. Arrête le scanner 
+   * 2. inverse l'orientation de la caméra entre "user" et "environment",
+   * 3. redémarre le scanner.
+   */
   const changeCamOrientation = async () => {
     stopScanner();
     setCamOrientation((current) =>
@@ -22,18 +31,25 @@ export default function Scan() {
     startScanner();
   };
 
+  /**
+   * Fonction pour initialiser et démarrer le scanner QR
+   * - Utilise la bibliothèque QrScanner pour décoder les codes QR depuis la caméra.
+   * - Définit un callback pour traiter les résultats du scan.
+   */
   const startScanner = async () => {
+
+    // Initialisation du scanner avec les options d'orientation de la caméra
     qrScannerRef.current = new QrScanner(
-      videoNodeRef.current,
+      videoNodeRef.current, // Élément vidéo où le flux de la caméra est affiché
       (result) => {
         console.log("decoded qr code:", result);
-        router.push(result.data);
+        router.push(result.data); // Redirige l'utilisateur vers l'URL contenue dans le QR code
       },
       {
         preferredCamera: camOrientation,
       }
     );
-
+    // Démarrage du scanner
     try {
       await qrScannerRef.current.start();
       console.log("Scanner started successfully.", qrScannerRef.current);
@@ -42,6 +58,10 @@ export default function Scan() {
     }
   };
 
+  /**
+  * Fonction pour arrêter le scanner QR
+  * - Arrête le flux vidéo et libère les ressources associées au scanner.
+  */
   const stopScanner = async () => {
     if (qrScannerRef.current) {
       await qrScannerRef.current.stop();
@@ -49,6 +69,7 @@ export default function Scan() {
     }
   };
 
+  // Démarrage du scanner à chaque fois que le composant est affiché 
   useEffect(() => {
     startScanner();
 
@@ -57,7 +78,7 @@ export default function Scan() {
         qrScannerRef.current.destroy();
       }
     };
-  }, [camOrientation, router]);
+  }, []);
 
   return (
     <>
